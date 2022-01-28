6195752f3afffe8faa42e2644866001ad5b556fd
Will Suttner <willsuttner@yahoo.com>
2022-01-27 16:39:53 -0800

    removed default objects and added wireframe with routing

---
+++ src/Portfolio.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Portfolio() {
+    return (
+        <div>
+            Links to Portfolio projects
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import About from './About';
+import Contact from './Contact';
+import Portfolio from './Portfolio';
+import Resume from './Resume';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path='/personal-site' element={<App />}>
+        <Route path='/personal-site/about' element={<About/>}/>  
+        <Route path='/personal-site/contact' element={<Contact/>}/>  
+        <Route path='/personal-site/portfolio' element={<Portfolio/>}/>  
+        <Route path='/personal-site/resume' element={<Resume/>}/>  
+      </Route>
+    </Routes>
+  </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
