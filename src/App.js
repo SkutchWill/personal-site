1c810d052cad0102c52d5a9737e18d7e813c7b43
Will Suttner <willsuttner@yahoo.com>
2022-01-28 17:13:00 -0800

    Added basic navigation bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import About from './About';
 import Contact from './Contact';
 import Portfolio from './Portfolio';
@@ -11,11 +13,10 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<Home/>}>
-          <Route path='/about' element={<About/>}/> 
-          <Route path='/contact' element={<Contact/>}/> 
-          <Route path='/portfolio' element={<Portfolio/>}/> 
-          <Route path='/resume' element={<Resume/>}/> 
-          <Route path='/contact' element={<Contact/>}/> 
+          <Route path='about' element={<About/>}/> 
+          <Route path='contact' element={<Contact/>}/> 
+          <Route path='portfolio' element={<Portfolio/>}/> 
+          <Route path='resume' element={<Resume/>}/> 
         </Route>                  
       </Routes>
     </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './styles/Navbar.css';
 
 export default function Navbar(props) {
     return (
-        <nav>Navbar</nav>
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <NavLink to='/'>Home</NavLink>
+                </div>
+                <div className='col'>
+                    <NavLink to='/about'>About</NavLink>
+                </div>
+                <div className='col'>
+                    <NavLink to='/contact'>Contact</NavLink>
+                </div>
+                <div className='col'>
+                    <NavLink to='/portfolio'>Portfolio</NavLink>
+                </div>
+                <div className='col'>
+                    <NavLink to='/resume'>Resume</NavLink>
+                </div>              
+            </div>
+
+        </div>
     );
 }
